c671a764e6afb5f46e05545d470d3f0075c8503b
Christian Budde Christensen <budde377@gmail.com>
2017-05-14 10:58:35 +0200

    feat: Add contact form

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,38 +1,61 @@
 /* ---------------------------------------------
  Contact form
  --------------------------------------------- */
-$(document).ready(function(){
-    $("#submit_btn").click(function(){
+$(document).ready(function () {
+  $('#contact_form').submit(function (event) {
+    event.preventDefault();
+    //get input field values
+    var user_name = $('input[name=name]').val()
+    var user_email = $('input[name=email]').val()
+    var user_message = $('textarea[name=message]').val()
 
-        //get input field values
-        var user_name = $('input[name=name]').val();
-        var user_email = $('input[name=email]').val();
-        var user_message = $('textarea[name=message]').val();
+    //simple validation at client's end
+    //we simply change border color to red if empty field using .css()
+    var proceed = true
+    if (user_name === '') {
+      $('input[name=name]').css('border-color', '#e41919')
+      proceed = false
+    }
+    if (user_email === '') {
+      $('input[name=email]').css('border-color', '#e41919')
+      proceed = false
+    }
 
-        //simple validation at client's end
-        //we simply change border color to red if empty field using .css()
-        var proceed = true;
-        if (user_name == "") {
-            $('input[name=name]').css('border-color', '#e41919');
-            proceed = false;
-        }
-        if (user_email == "") {
-            $('input[name=email]').css('border-color', '#e41919');
-            proceed = false;
-        }
+    if (user_message === '') {
+      $('textarea[name=message]').css('border-color', '#e41919')
+      proceed = false
+    }
 
-        if (user_message == "") {
-            $('textarea[name=message]').css('border-color', '#e41919');
-            proceed = false;
-        }
+    //everything looks good! proceed...
+    if (proceed) {
+      //data to be sent to server
+      var post_data = {
+        'name': user_name,
+        'email': user_email,
+        '_replyto': user_email,
+        'message': user_message
+      }
+      $.ajax({
+        url: 'https://formspree.io/'+$('input[name=receiver]').val(),
+        method: 'POST',
+        data: post_data,
+        dataType: 'json'
+      })
+      var output = '<div class="success">Beskeden er sendt!</div>'
 
-        return proceed;
-    });
+      //reset values in all input fields
+      $('#contact_form input').val('')
+      $('#contact_form textarea').val('')
+      $('#result').hide().html(output).slideDown()
 
-    //reset previously set border colors and hide all message on .keyup()
-    $("#contact_form input, #contact_form textarea").keyup(function(){
-        $("#contact_form input, #contact_form textarea").css('border-color', '');
-        $("#result").slideUp();
-    });
+    }
 
-});
+  })
+
+  //reset previously set border colors and hide all message on .keyup()
+  $('#contact_form input, #contact_form textarea').keyup(function () {
+    $('#contact_form input, #contact_form textarea').css('border-color', '')
+    $('#result').slideUp()
+  })
+
+})
--- js/prices-form.js
+++ js/prices-form.js
@@ -2,8 +2,8 @@
  Contact form
  --------------------------------------------- */
 $(document).ready(function(){
-    $("#submit_btn").click(function(){
-
+    $("#contact_form").submit(function(event){
+        event.preventDefault()
         //get input field values
         var company_name = $('input[name=name]').val();
         var user_email = $('input[name=email]').val();
@@ -42,7 +42,31 @@ $(document).ready(function(){
             proceed = false;
         }
 
-        return proceed;
+        if (proceed) {
+          //data to be sent to server
+          var post_data = {
+            'name': user_name,
+            'email': user_email,
+            'phone': user_number,
+            '_replyto': user_email,
+            'company_name': company_name,
+            'company_employees': company_employees,
+            'company_country': company_country
+          }
+          $.ajax({
+            url: 'https://formspree.io/'+$('input[name=receiver]').val(),
+            method: 'POST',
+            data: post_data,
+            dataType: 'json'
+          })
+          var output = '<div class="success">Beskeden er sendt!</div>'
+
+          //reset values in all input fields
+          $('#contact_form input').val('')
+          $('#contact_form textarea').val('')
+          $('#result').hide().html(output).slideDown()
+
+        }
     });
 
     //reset previously set border colors and hide all message on .keyup()
