c04e2aceac3ba4c4982317a9a41cb946c037e704
Christian Budde Christensen <budde377@gmail.com>
2017-05-14 10:01:37 +0200

    feat: Remove rs-plugin and using formspree

--- js/prices-form.js
+++ js/prices-form.js
@@ -9,9 +9,9 @@ $(document).ready(function(){
         var user_email = $('input[name=email]').val();
         var user_number = $('input[name=number]').val();
         var user_name = $('input[name=name]').val();
-        var company_employees $('select[name=options]').val();
-        var company_country $('input[name=country]').val();
-        
+        var company_employees = $('select[name=options]').val();
+        var company_country = $('input[name=country]').val();
+
         //simple validation at client's end
         //we simply change border color to red if empty field using .css()
         var proceed = true;
@@ -23,7 +23,7 @@ $(document).ready(function(){
             $('input[name=email]').css('border-color', '#e41919');
             proceed = false;
         }
-        
+
         if (user_number == "") {
             $('input[name=number]').css('border-color', '#e41919');
             proceed = false;
@@ -42,49 +42,13 @@ $(document).ready(function(){
             proceed = false;
         }
 
-
-        //everything looks good! proceed...
-        if (proceed) {
-            //data to be sent to server
-            post_data = {
-                'companyName': company_name,
-                'userEmail': user_email,
-                'userNumber': user_number,
-                'userName': user_name,
-                'companyEmployees': company_employees,
-                'companyCountry': company_country
-            };
-            
-            //Ajax post data to server
-            $.post('pricing_contact_me.php', post_data, function(response){
-            
-                //load json data from server and output message     
-                if (response.type == 'error') {
-                    output = '<div class="error">' + response.text + '</div>';
-                }
-                else {
-                
-                    output = '<div class="success">' + response.text + '</div>';
-                    
-                    //reset values in all input fields
-                    $('#contact_form input').val('');
-                    $('#contact_form textarea').val('');
-                    $('#contact_form selected').val('Antal ansatte');
-
-                }
-                
-                $("#result").hide().html(output).slideDown();
-            }, 'json');
-            
-        }
-        
-        return false;
+        return proceed;
     });
-    
+
     //reset previously set border colors and hide all message on .keyup()
     $("#contact_form input, #contact_form textarea").keyup(function(){
         $("#contact_form input, #contact_form textarea").css('border-color', '');
         $("#result").slideUp();
     });
-    
+
 });
